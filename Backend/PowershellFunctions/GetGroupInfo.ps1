803a11dd48c0ad8308e075042292894aad8b78ff
detf0r <ekimenkoip@gmail.com>
2023-06-04 19:25:36 +0700

    members in GroupInfo remove GroupMembers UserCreation

--- Backend/PowershellFunctions/GetGroupInfo.ps1
+++ Backend/PowershellFunctions/GetGroupInfo.ps1
@@ -2,8 +2,15 @@ param([string] $GroupID)
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 $groupInfo = Get-ADGroup -identity $GroupID -ErrorAction ignore
 if ( $null -ne $groupInfo ) {
-    $group = Get-ADGroup -identity $GroupID  | select-object SamAccountName,DistinguishedName,GroupCategory,GroupScope
-    return $group
+    $info = "
+            Name: $($groupInfo  | Select-Object -expand SamAccountName) 
+            Container: $($groupInfo  | Select-Object -expand DistinguishedName)  
+            Type: $($groupInfo  | Select-Object -expand GroupCategory)  
+            Group Scope: $($groupInfo  | Select-Object -expand GroupScope)"
+            $members = "
+            Members: " + (Get-ADGroupMember $GroupLogin.Text | Select-Object -expand Name)
+            $info =  $info + $members
+    return $info
 }
 else {
     return $GroupID
