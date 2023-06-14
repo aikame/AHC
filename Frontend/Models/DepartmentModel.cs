71b4d5be0ea3fcef75cea29e870337c2451b7d10
aikame <asus1172@gmail.com>
2023-06-15 01:49:02 +0700

    models for db

---
+++ Frontend/Models/DepartmentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Frontend.Models
+{
+    public class DepartmentModel
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string NameRus { get; set; }
+    }
+}
