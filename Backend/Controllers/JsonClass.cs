7a07ebbf31f2e221478a22d97aac77467f8bdc28
aikame <44811230+aikame@users.noreply.github.com>
2023-06-26 17:17:23 +0700

    Create JsonClass.cs
    пиздец, гит гавно

---
+++ Backend/Controllers/JsonClass.cs
@@ -0,0 +1,13 @@
+namespace Backend.JSON
+{
+    public class ChangePasswd
+    {
+        string UserLogin,
+            Password;
+    }
+    public class UserGroup
+    {
+        string UserLogin,
+            GroupLogin;
+    }
+}
