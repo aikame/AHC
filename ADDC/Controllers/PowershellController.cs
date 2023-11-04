1486ef4f01232eb7be5e9c82b6ae1cb4f6486835
detf0r <ekimenkoip@gmail.com>
2023-11-04 14:20:20 +0700

    Creating new project

---
+++ ADDC/Controllers/PowershellController.cs
@@ -0,0 +1,22 @@
+﻿using ADDC.Services;
+using ActiveDirectory.Services;
+using Newtonsoft.Json;
+using Powershell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Web.Http;
+
+using Powershell.Abstractions;
+
+namespace ADDC.Controllers
+{
+    [ConnectorAuthorize]
+    public class PowershellController : BaseController 
+    {
+    }
+}
