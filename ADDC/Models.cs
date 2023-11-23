fedea6ba03b4ebb6e908dcf257e906fec7bf7e5d
detf0r <ekimenkoip@gmail.com>
2023-11-23 20:44:34 +0700

    add user model

---
+++ ADDC/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace ADDC.Models
+{
+    public class UserModel
+    {
+        public string name, RUname,
+               surname, RUsurname,
+               midname, RUmidname,
+               city, RUcity,
+               company, RUcompany, 
+               department, RUdepartment,
+               appointment, RUappointment;
+    }
+}
