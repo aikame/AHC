1cea3707056cc3c474e5e542d6e0586175b800a9
Георгий Крюков <gkryukov-ki21@stud.sfu-kras.ru>
2024-07-16 23:16:40 +0700

    login screen beauties

--- Frontend/hello/urls.py
+++ Frontend/hello/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from hello import views
 
 urlpatterns = [
+
     path("", views.home, name="home"),
+    #path("", views.home, name="home"),
     path("settings", views.settings, name="settings"),
     path("employee/<str:id>", views.employee, name="employee"),
     path("computer", views.computer, name="computer"),
