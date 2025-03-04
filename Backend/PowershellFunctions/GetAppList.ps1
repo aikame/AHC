90be67276750247b00d0c37635a8ba8571ddff36
detf0r <ekimenkoip@gmail.com>
2025-03-04 13:52:16 +0700

    Все равно не пишет размер((

--- Backend/PowershellFunctions/GetAppList.ps1
+++ Backend/PowershellFunctions/GetAppList.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser
-$apps = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, Size, InstallDate | sort-object displayname | Where-Object {$_.DisplayName -ne $null }
+$apps = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | sort-object displayname | Where-Object {$_.DisplayName -ne $null }
 
 $list = "{AppList: $($apps | ConvertTo-Json)}"
 return $list
