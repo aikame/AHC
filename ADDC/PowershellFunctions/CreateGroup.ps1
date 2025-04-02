8ebab14e2e59fbd597f2e072c2cc41c1245515a9
detf0r <ekimenkoip@gmail.com>
2025-04-02 18:44:56 +0700

    Гига-скрипт

---
+++ ADDC/PowershellFunctions/CreateGroup.ps1
@@ -0,0 +1,5 @@
+﻿param([string] $grpName, $Description)
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+$group = New-ADGroup -Name $grpName -GroupCategory Security -GroupScope Global -Description $Description
+$result = Get-ADGroup $grpName -property Description | ConvertTo-Json
+return $result
---
+++ ADDC/PowershellFunctions/GetGroupMembers.ps1
@@ -0,0 +1,8 @@
+﻿param([string] $GroupID)
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+$groupMembers = Get-ADGroupMember $GroupID | ForEach-Object {
+    Get-ADUser -Identity $_.DistinguishedName -Properties Name, extensionAttribute1, extensionAttribute2, extensionAttribute3, Title | Select-Object Name, extensionAttribute1, extensionAttribute2, extensionAttribute3, Title
+}
+$list = "{Members: $($groupMembers | ConvertTo-Json)}"
+return $list
