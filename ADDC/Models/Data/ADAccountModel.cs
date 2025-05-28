947e46467565f9806fcc4cf2c67ccc79003d1e7d
detf0r <ekimenkoip@gmail.com>
2025-05-28 21:31:44 +0700

    fix

--- ADDC/Models/Data/ADAccountModel.cs
+++ ADDC/Models/Data/ADAccountModel.cs
@@ -5,9 +5,9 @@
         public string? DistinguishedName { get; set; }
         public string? SamAccountName { get; set; }
         public string? EmailAddress { get; set; }
-        public bool Enabled { get; set; }
-        public bool PasswordExpired { get; set; }
-        public DateTime PasswordLastSet { get; set; }
+        public bool? Enabled { get; set; }
+        public bool? PasswordExpired { get; set; }
+        public DateTime? PasswordLastSet { get; set; }
         public List<string>? MemberOf { get; set; }
     }
 }
--- Backend/Services/AccountService.cs
+++ Backend/Services/AccountService.cs
@@ -77,7 +77,7 @@ namespace Backend.Services
                 ComputerModel? computer = await _computerService.FindDCinDomain(account.Domain);
                 if (computer is null) { return false; }
 
-                var jsonContent = new StringContent(JObject.FromObject(account).ToString(), Encoding.UTF8, "application/json");
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(account), Encoding.UTF8, "application/json");
                 var result = await _client.PostAsync("https://" +computer.IPAddress + ":" + _connectorPort + "/BanUser", jsonContent);
                 
                 return result.IsSuccessStatusCode; 
